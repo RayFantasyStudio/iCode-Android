1bdb085af6bf8bd3d5fe2f6466c40d69ba92c3ef
AllenTom <qweasdzxczse2@gmail.com>
2016-02-02 11:24:04 +0800

    优化界面2

--- app/src/main/kotlin/com/rayfantasy/icode/ui/activity/UserActivity.kt
+++ app/src/main/kotlin/com/rayfantasy/icode/ui/activity/UserActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
 import com.android.volley.Request
 import com.rayfantasy.icode.R
 import com.rayfantasy.icode.postutil.PostUtil
 import com.rayfantasy.icode.ui.adapter.UserListAdapter
 import kotlinx.android.synthetic.main.activity_user.*
 import kotlinx.android.synthetic.main.content_user.*
+import org.jetbrains.anko.alert
 import java.util.*
 
 class UserActivity : AppCompatActivity() {
@@ -66,5 +69,30 @@ class UserActivity : AppCompatActivity() {
             t.printStackTrace()
         }
         )
+    }
+    fun changeUserIcon(){
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.user_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item!!.itemId
+        when (id){
+            R.id.account_menu_out ->{
+                alert("确定退出账户？","iCode") {
+                    positiveButton("确定") {  PostUtil.logoutUser(); finish() }
+                    negativeButton("手滑了") {  }
+                }.show()
+
+                return true
+            }
+            R.id.account_menu_usericon ->{changeUserIcon()
+                return true}
+        }
+        return true
     }
 }
