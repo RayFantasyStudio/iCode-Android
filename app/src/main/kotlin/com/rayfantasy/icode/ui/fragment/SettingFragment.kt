e3ad1e72f25e1c67f3411ef93bebb27f117888c3
ztc1997 <2455484588@qq.com>
2016-02-06 13:50:01 +0800

    SettingFragment: 修正许可证点击监听

--- app/src/main/kotlin/com/rayfantasy/icode/ui/fragment/SettingFragment.kt
+++ app/src/main/kotlin/com/rayfantasy/icode/ui/fragment/SettingFragment.kt
@@ -1,37 +1,22 @@
 package com.rayfantasy.icode.ui.fragment
 
 import android.os.Bundle
-import android.preference.PreferenceFragment
 import android.preference.Preference
 import android.preference.Preference.OnPreferenceClickListener
-
+import android.preference.PreferenceFragment
 import com.rayfantasy.icode.R
-
 import de.psdev.licensesdialog.LicensesDialog
 
 class SettingFragment : PreferenceFragment() {
-
     private lateinit var license: Preference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*addPreferencesFromResource(R.xml.settings)*/
-        license = findPreference("licenses") as Preference
+        addPreferencesFromResource(R.xml.settings)
+        license = findPreference("licenses")
         license.onPreferenceClickListener = OnPreferenceClickListener {
-            val PreKey = "licenses"
-            if (PreKey.equals(license)) {
-                LicensesDialog.Builder(activity).setNotices(R.raw.notices).setIncludeOwnLicense(true).build().show()
-            }
-            false
+            LicensesDialog.Builder(activity).setNotices(R.raw.notices).setIncludeOwnLicense(true).build().show()
+            true
         }
     }
-    /* class PreferenceClick {
-         fun onPreferenceClick(preference: Preference): Boolean {
-             String P = license.getKey().toString
-             if (preference === license) {
-                 LicensesDialog.Builder(getAtNotices(R.raw.notices).setIncludeOwnLicense(true).build().show())
-             }
-             return true
-         }
-     }*/
 }
