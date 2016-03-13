eba3a25401354a7b313fe9d1003faf3a9ae403f9
ztc1997 <2455484588@qq.com>
2016-03-14 00:16:52 +0800

    FavoriteFragment: 修复bug #3

--- app/src/main/kotlin/com/rayfantasy/icode/ui/fragment/FavoriteFragment.kt
+++ app/src/main/kotlin/com/rayfantasy/icode/ui/fragment/FavoriteFragment.kt
@@ -6,10 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.android.volley.Request
-import com.raizlabs.android.dbflow.sql.language.SQLite
+import com.raizlabs.android.dbflow.sql.language.Join
+import com.raizlabs.android.dbflow.sql.language.NameAlias
+import com.raizlabs.android.dbflow.sql.language.Select
 import com.rayfantasy.icode.R
-import com.rayfantasy.icode.databinding.FragmentMainBinding
-import com.rayfantasy.icode.model.ICodeTheme
+import com.rayfantasy.icode.extra.UpdateAbleList
 import com.rayfantasy.icode.postutil.PostUtil
 import com.rayfantasy.icode.postutil.bean.CodeGood
 import com.rayfantasy.icode.postutil.bean.CodeGood_Table
@@ -17,16 +18,15 @@ import com.rayfantasy.icode.postutil.bean.Favorite
 import com.rayfantasy.icode.postutil.bean.Favorite_Table
 import com.rayfantasy.icode.ui.adapter.CodeListAdapter
 import com.rayfantasy.icode.ui.adapter.LoadMoreAdapter
-import kotlinx.android.synthetic.main.fragment_favorite.*
 import kotlinx.android.synthetic.main.fragment_favorite.view.*
-import org.apache.commons.collections4.list.SetUniqueList
 import org.jetbrains.anko.support.v4.onRefresh
 
 class FavoriteFragment : FragmentBase() {
     companion object {
         const val LOAD_ONCE = 10
     }
-    private lateinit var adapter: CodeListAdapter
+
+    private val adapter by lazy { CodeListAdapter(activity, UpdateAbleList(getCacheData())) { loadCodeGoods(false) } }
     private val isRefreshing: Boolean
         get() = request != null
     private var request: Request<*>? = null
@@ -39,45 +39,45 @@ class FavoriteFragment : FragmentBase() {
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.fragment_favorite,container,false)
+        return inflater?.inflate(R.layout.fragment_favorite, container, false)
     }
 
     private fun initRecyclerView() {
         val layoutManager = LinearLayoutManager(activity)
         view.favorite_recycler_view.layoutManager = layoutManager
-        adapter = CodeListAdapter(activity, SetUniqueList.setUniqueList(getCacheData())) { loadCodeGoods(false) }
         view.favorite_recycler_view.adapter = adapter
     }
+
     private fun loadCodeGoods(refresh: Boolean) {
         //如果正在刷新，则不再发起新的刷新请求
         if (isRefreshing)
             return
 
         //生成加载条件，目前加载3个，方便测试
-        if(PostUtil.user == null){
+        if (PostUtil.user == null) {
             adapter.footerState = LoadMoreAdapter.FOOTER_STATE_FAILED
             return
         }
-        val condition = "SELECT a.* FROM icode.code_good a JOIN (SELECT * FROM icode.favorite WHERE userId = ${PostUtil.user!!.id}) b on a.id = b.goodId ORDER BY b.createat"
+        val condition = "JOIN (SELECT * FROM favorite WHERE userId = ${PostUtil.user!!.id}) b on a.id = b.goodId ORDER BY b.createat DESC"
         request = PostUtil.selectCodeGood(condition) {
             onSuccess {
                 if (isDetached) return@onSuccess
                 view.favo_swipe.isRefreshing = false
                 request = null
 
-                if (it.isEmpty() ) {
+                //if (it.isEmpty() ) {
                     //如果结果为空，则表示没有更多内容了
                     adapter.footerState = LoadMoreAdapter.FOOTER_STATE_NO_MORE
-                } else {
+                //} else {
                     if (refresh) {
                         adapter.codeGoods.clear()
                     }
                     //否则将结果加入codeGoods，并刷新adapter
                     adapter.codeGoods.addAll(it)
                     if (refresh) adapter.notifyDataSetChanged()
-                    else adapter.notifyItemRangeInserted(adapter.itemCount - 1 - it.size, it.size)
+                    else adapter.notifyDataSetChanged()
                     cacheData(adapter.codeGoods)
-                }
+                //}
                 onFailed { t, rc ->
                     request = null
                     if (isDetached || view == null) return@onFailed
@@ -93,15 +93,20 @@ class FavoriteFragment : FragmentBase() {
         PostUtil.cancel(request)
         request = null
     }
+
     fun cacheData(data: List<CodeGood>) {
         data.forEach {
             it.loadContentFromCache()
             it.save()
         }
     }
-    fun getCacheData() = SQLite.select(CodeGood_Table.id)
-            .from(CodeGood::class.java).leftOuterJoin(Favorite::class.java)
-            .on(CodeGood_Table.id.withTable().eq(Favorite_Table.goodId.withTable()))
+
+    fun getCacheData() = Select()
+            .from(CodeGood::class.java).`as`("a")
+            .join(Favorite::class.java, Join.JoinType.CROSS).`as`("b")
+            .on(CodeGood_Table.id.withTable(NameAlias("a")).eq(Favorite_Table.goodId.withTable(NameAlias("b"))))
+            .where(CodeGood_Table.id.withTable(NameAlias("a")).`is`(PostUtil.user!!.id))
+            .orderBy(Favorite_Table.createat, false)
             .queryList()
 
 }
